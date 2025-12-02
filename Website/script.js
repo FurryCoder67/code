49c9c7705a422339b2b779d1823f291072ffdbde
Aiden <aidenksyip@gmail.com>
2025-12-01 19:21:39 -0500

    brainrot clicker

--- Website/script.js
+++ Website/script.js
@@ -1,12 +1,18 @@
 let clicks = 0;
 
 const shopItems = [
-    { name: "Trippi Troppi", cost: 10, cps: 1, img: "trippitroppi.webp" },
-    { name: "Tung Tung Tung Sahur", cost: 50, cps: 5, img: "Tung-Tung-Tung-Sahur.webp" },
-    { name: "Tralalero Tralala", cost: 100, cps: 10, img: "Tralalero_Tralalala.webp" },
-    { name: "Bombardino Crocodilo", cost: 500, cps: 30, img: "bombardino-crocodilo.webp" },
-    { name: "Brr Brr Patabim", cost: 1000, cps: 100, img: "brr-brr-patapim.webp" },
-    { name: "Divine Cappuccino Assassino", cost: 999999999, cps: 999999999, img: "300px-Fire_katanas.webp" }
+    { name: "Trippi Troppi", cost: 10, cps: 1, img: "/pics/trippitroppi.webp" },
+    { name: "Tung Tung Tung Sahur", cost: 50, cps: 5, img: "/pics/Tung-Tung-Tung-Sahur.webp" },
+    { name: "Tralalero Tralala", cost: 100, cps: 10, img: "/pics/Tralalero_Tralalala.webp" },
+    { name: "Bombardino Crocodilo", cost: 500, cps: 30, img: "/pics/bombardino-crocodilo.webp" },
+    { name: "Brr Brr Patabim", cost: 1000, cps: 100, img: "/pics/brr-brr-patapim.webp" },
+    { name: "Frulli Frulla", cost: 2000, cps: 200, img: "/pics/Frulli-Frulla-PNG-Pic.png" },
+    { name: "Chimpanzini Bananini", cost: 10000, cps: 500, img: "/pics/300px-Chimpanzini_Bananino.webp" },
+    { name: "Odin din din dun", cost: 20000, cps: 1000, img: "/pics/odindindoon.webp" },
+    { name: "Liri Liri Larila", cost: 100000, cps: 5000, img: "/pics/Lirili_Larila.jpeg.jpeg" },
+    { name: "Lavaka Saturno Saturnita", cost: 500000, cps: 10000, img: "/pics/lavakasuturnosaturnita.jpg" },
+    { name: "Frigo Camelo", cost: 10000000, cps: 1000000, img: "/pics/frigo-camelo.webp" },
+    { name: "Divine Cappuccino Assassino", cost: 999999999, cps: 999999999, img: "/pics/300px-Fire_katanas.webp" }
 ];
 let cps = 0;
 
